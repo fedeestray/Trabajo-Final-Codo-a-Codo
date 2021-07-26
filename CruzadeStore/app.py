bce85e93211df1035246180f73452f676100bbc9
fedeestray <48691016+fedeestray@users.noreply.github.com>
2021-07-26 15:02:38 -0300

    Add files via upload

--- CruzadeStore/app.py
+++ CruzadeStore/app.py
@@ -15,119 +15,17 @@ app.config['MYSQL_DATABASE_DB'] = 'cruzade'
 mysql.init_app(app)
 
 
-carpeta = os.path.join('uploads')
+carpeta = os.path.join('img')
 app.config['carpeta'] = carpeta
 
 #%% Uploads
-@app.route('/uploads/<nombreFoto>')
+@app.route('/img/<nombreFoto>')
 def uploads(nombreFoto):
     return send_from_directory(app.config['carpeta'], nombreFoto)
-#%% Index empleados
+#%%
+#%% Home
 @app.route('/')
 def index():
-    sql = "SELECT * FROM `cruzade`.`empleados`;"
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    cursor.execute(sql)    
-        
-    empleados = cursor.fetchall()
-    print('Información de la base de datos:\n',empleados)    
-    
-    conn.commit()       
-    return render_template('empleados/index.html', empleados = empleados)
-#%% Create empleados
-@app.route('/create')
-def create():
-    return render_template('empleados/create.html')
-#%% Storage empleados
-@app.route('/store',  methods=['POST'])     
-# cuando formulario create.hmtl hace el submit envia los datos a /store
-
-def storage():                             
-    _nombre=request.form['txtNombre'] 
-    _correo=request.form['txtCorreo']  
-    _foto=request.files['txtFoto']     
-    
-    now=datetime.now()
-    tiempo=now.strftime("%Y%H%M%S")
-
-    if _foto.filename !='':
-        nuevoNombreFoto=tiempo+_foto.filename
-        _foto.save("uploads/"+nuevoNombreFoto)
-    
-    sql="INSERT INTO `cruzade`.`empleados` (`nomap`,`correo`,`foto`) VALUES (%s,%s,%s)" #Creo la sentencia sql
-    datos=(_nombre,_correo,nuevoNombreFoto)
-
-    conn=mysql.connect()
-    cursor=conn.cursor()
-    cursor.execute(sql,datos)               # ejecuta la sentencia sql 
-    conn.commit()
-    
-    return redirect('/')  # y renderiza index.html
-#%% Destroy empleados
-@app.route('/destroy/<int:id>')
-def destroy(id):
-    sql="DELETE  FROM `cruzade`.`empleados` WHERE  id=%s;"     
-    conn=mysql.connect()              # me conecto a la base de datos
-    cursor=conn.cursor()              # almacena informacion 
-    
-    cursor.execute("SELECT foto FROM `cruzade`.`empleados` WHERE id=%s",(id)) 
-
-    fila=cursor.fetchall()   # fila va a tener un solo registro y 1 solo campo
-    os.remove(os.path.join(app.config['carpeta'],fila[0][0]))
-        
-    cursor.execute(sql,(id))               # ejecuto en MySQL la variable sql
-    conn.commit()
-    return redirect('/')                 # redirecciona al 'index de empleados'
-
-#%% Edit empleados
-@app.route('/edit/<int:id>')
-def edit(id): 
-    sql="SELECT * FROM `cruzade`.`empleados` WHERE id=%s;" 
-    conn=mysql.connect()              
-    cursor=conn.cursor()               
-    cursor.execute(sql,(id))          
-    empleados=cursor.fetchall() 
-    conn.commit()
-    print(empleados) 
-    return render_template('empleados/edit.html',empleados=empleados) 
-#%% Update empleados
-@app.route('/update',  methods=['POST']) 
-
-# cuando formulario create.hmtl hace el submit envia los datos a /store
-def  update():                             
-    _nombre=request.form['txtNombre']  
-    _correo=request.form['txtCorreo']  
-    _foto=request.files['txtFoto']     
-    id=request.form['txtId']
-
-    sql="UPDATE `cruzade`.`empleados` SET  `nomap`=%s  ,`correo`=%s WHERE id=%s" 
-    datos=(_nombre,_correo,id)  
-    conn=mysql.connect()
-    cursor=conn.cursor()
-
-# agregamos
-    now=datetime.now()
-    tiempo=now.strftime("%Y%H%M%S")
-
-    if _foto.filename !='':  
-        nuevoNombreFoto=tiempo+_foto.filename
-        _foto.save("uploads/"+nuevoNombreFoto)
-   
-        cursor.execute("SELECT foto FROM `cruzade`.`empleados` WHERE id=%s",(id)) 
-
-        fila=cursor.fetchall()   
-        os.remove(os.path.join(app.config['carpeta'],fila[0][0]))
-        cursor.execute("UPDATE `cruzade`.`empleados` SET foto=%s WHERE id=%s",(nuevoNombreFoto,id)) 
-        conn.commit()
-
-    cursor.execute(sql,datos)               
-    conn.commit()
-    return redirect('/')  # y renderiza index.html de empleados
-
-#%% Index productos
-@app.route("/productos/")
-def productIndex():
     sql = "SELECT * FROM `cruzade`.`productos`;"
     conn = mysql.connect()
     cursor = conn.cursor()
@@ -137,15 +35,44 @@ def productIndex():
     print('Información de la base de datos:\n',productos)    
     
     conn.commit()       
-    return render_template('productos/index.html', productos = productos) #Renderiza el index de productos
+    #return render_template('productos.html', productos = productos) #Renderiza el index de productos 
+    return render_template('index.html', productos = productos)
+#%%
+
+#%% Acerca de
+@app.route('/Acerca_de')
+def acerca():
+    return render_template('Acerca_de.html')
+#%%
+#%% Contacto
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
+#%%
+#%% Gestion
+@app.route('/gestion/')
+def gestion():
+    sql = "SELECT * FROM `cruzade`.`productos`;"
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql)    
+        
+    productos = cursor.fetchall()
+    print('Información de la base de datos:\n',productos)    
+    
+    conn.commit()       
+    return render_template('gestion.html', productos = productos) 
+#%%
 #%% Create productos
 @app.route('/productos/create')
 def productCreate():
     return render_template('productos/create.html') #Renderiza create de productos
+#%%
+
 #%% Storage productos
 @app.route('/productos/store',  methods=['POST'])
-# cuando formulario create.hmtl hace el submit envia los datos a /store de productos
-def productStorage():                             
+
+def storage():                             
     _nombre=request.form['txtNombre']  
     _precio=request.form['txtPrecio']  
     _foto=request.files['txtFoto']     
@@ -155,7 +82,7 @@ def productStorage():
 
     if _foto.filename !='':
         nuevoNombreFoto=tiempo+_foto.filename
-        _foto.save("uploads/"+nuevoNombreFoto)
+        _foto.save("img/"+nuevoNombreFoto)
     
     sql="INSERT INTO `cruzade`.`productos` (`nomprod`,`precio`,`foto`) VALUES (%s,%s,%s)" 
     datos=(_nombre,_precio,nuevoNombreFoto) 
@@ -164,10 +91,11 @@ def productStorage():
     cursor=conn.cursor()
     cursor.execute(sql,datos)               
     conn.commit()
-    return redirect('/productos/')  # y renderiza index.html de productos
+    return redirect('/gestion')  
+#%%
 #%% Edit productos
 @app.route('/productos/edit/<int:id>')
-def productEdit(id): 
+def edit(id): 
     sql="SELECT * FROM `cruzade`.`productos` WHERE id=%s;" 
     conn=mysql.connect()              
     cursor=conn.cursor()              
@@ -175,7 +103,9 @@ def productEdit(id):
     productos=cursor.fetchall() 
     conn.commit()
     print(productos) 
-    return render_template('productos/edit.html',productos=productos) 
+    return render_template('/productos/edit.html',productos=productos)
+#%%
+
 #%% Update productos
 @app.route('/productos/update',  methods=['POST']) 
 
@@ -197,7 +127,7 @@ def productosUpdate():
 
     if _foto.filename !='':  
         nuevoNombreFoto=tiempo+_foto.filename
-        _foto.save("uploads/"+nuevoNombreFoto)
+        _foto.save("img/"+nuevoNombreFoto)
    
         cursor.execute("SELECT foto FROM `cruzade`.`productos` WHERE id=%s",(id)) 
 
@@ -208,7 +138,8 @@ def productosUpdate():
 
     cursor.execute(sql,datos)               
     conn.commit()
-    return redirect('/productos/')  #Renderiza index.html de productos
+    return redirect('/gestion')
+#%%
 
 #%% Destroy productos
 @app.route('/productos/destroy/<int:id>')
@@ -223,8 +154,43 @@ def productDdestroy(id):
         
     cursor.execute(sql,(id)) 
     conn.commit()
-    return redirect('/productos/')                 # reidrecciona al '/' de productos
+    return redirect('/gestion')
+#%%
 
+#%% Compra 
+@app.route('/compra')
+def compra():
+    return render_template('compra.html')
+#%%
+
+#%% Login
+@app.route('/login')
+def loginpage():
+    return render_template('login.html')
+#%%
+
+#%% Login ingreso
+@app.route('/form_login', methods=['POST', 'GET'])
+def login():
+    name1 = request.form['username']
+    pwd = request.form['password']
+
+    sql = 'SELECT correo,password FROM `cruzade`.`empleados`;'
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    database = cursor.fetchall()
+    dictDatabase = dict(database)
+    print(dictDatabase)
+
+    conn.commit()
+    if name1 not in dictDatabase:
+        return render_template('login.html', info = 'Usuario invalido')
+    else:
+        if dictDatabase[name1] != pwd:
+            return render_template('login.html', info = 'Contraseña invalida, si el problema persiste puede que no sea empleado, contactese con su superior')            
+        else:
+            return redirect('/gestion')
 #%%
 if __name__ == '__main__':
     app.run(debug = True)
--- CruzadeStore/cruzade.sql
+++ CruzadeStore/cruzade.sql
@@ -2,10 +2,10 @@
 -- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 16, 2021 at 07:33 PM
--- Server version: 10.4.19-MariaDB
--- PHP Version: 8.0.6
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 26-07-2021 a las 20:00:47
+-- Versión del servidor: 10.4.19-MariaDB
+-- Versión de PHP: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,36 +18,34 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `cruzade`
+-- Base de datos: `cruzade`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `empleados`
+-- Estructura de tabla para la tabla `empleados`
 --
 
 CREATE TABLE `empleados` (
   `id` int(10) NOT NULL,
   `nomap` varchar(255) NOT NULL,
   `correo` varchar(255) NOT NULL,
-  `foto` varchar(5000) NOT NULL
+  `password` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `empleados`
+-- Volcado de datos para la tabla `empleados`
 --
 
-INSERT INTO `empleados` (`id`, `nomap`, `correo`, `foto`) VALUES
-(1, 'Carlos Martinez', 'carlosmartinez@yahoo.com.ar', '2021131841Carlos_Martínez.jpg'),
-(2, 'Martin Yanes', 'martinyanes@gmail.com', '2021131954Martin_Yanes.jpg'),
-(3, 'Julieta Marino', 'julimarino@outlook.com', '2021132047Julieta_Marino.jpg'),
-(4, 'Mariela Laudecina', 'marilau@gmail.com', '2021132243Mariela_Laudecina.jpg');
+INSERT INTO `empleados` (`id`, `nomap`, `correo`, `password`) VALUES
+(1, 'Carlos Martinez', 'carlosmartinez@cruzade.com.ar', 'carlos123'),
+(3, 'Mario Chavez', 'maritochavez@cruzade.com.ar', 'marito123');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `productos`
+-- Estructura de tabla para la tabla `productos`
 --
 
 CREATE TABLE `productos` (
@@ -58,41 +56,41 @@ CREATE TABLE `productos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `productos`
+-- Volcado de datos para la tabla `productos`
 --
 
 INSERT INTO `productos` (`id`, `nomprod`, `precio`, `foto`) VALUES
-(1, 'Auriculares Daewoo inalámbricos TWS DW-304IE-BK', 2899, '2021141854auridaewooverde.jpg'),
-(2, 'Auricular Inalambrico Jbl Tune 510 Bt On Ear', 6887, '2021140847aurijbltune.jpg');
+(1, 'Apple Airpods Pro', 42999, '2021143956airpods.jpg'),
+(2, 'Auriculares Daewoo inalámbricos TWS DW-304IE-BK', 2999, '20211440112021141854auridaewooverde.jpg');
 
 --
--- Indexes for dumped tables
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `empleados`
+-- Indices de la tabla `empleados`
 --
 ALTER TABLE `empleados`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `productos`
+-- Indices de la tabla `productos`
 --
 ALTER TABLE `productos`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT for table `empleados`
+-- AUTO_INCREMENT de la tabla `empleados`
 --
 ALTER TABLE `empleados`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `productos`
+-- AUTO_INCREMENT de la tabla `productos`
 --
 ALTER TABLE `productos`
   MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
